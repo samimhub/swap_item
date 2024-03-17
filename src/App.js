3e82259564d0add85c8193d45e14f27d63d713fb
samim <aktarsamim529@gmail.com>
2024-03-18 02:05:46 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [list1,setList1] = useState([
+    {title:'Item 1', checked:false},
+    {title:'Item 2', checked:false},
+    {title:'Item 3', checked:false}
+  ])
+
+  const [list2,setList2] = useState([
+    {title:'Item A', checked:false},
+    {title:'Item B', checked:false},
+    {title:'Item C', checked:false}
+  ])
+
+  const handleCheckboxChange = (index) => {
+    const updatedList1 = [...list1];
+    updatedList1[index].checked = !updatedList1[index].checked;
+    setList1(updatedList1);
+  }
+
+  const handleSwap = () => {
+    const updatedList1 = [...list1];
+    const updatedList2 = [...list2];
+    updatedList1.forEach((item, index) => {
+      if (item.checked) {
+        const temp = updatedList1[index].title;
+        updatedList1[index].title = updatedList2[index].title;
+        updatedList2[index].title = temp;
+      }
+      updatedList1[index].checked = false;
+    });
+    setList1(updatedList1);
+    setList2(updatedList2);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h3>List 1</h3>
+        <ul>
+          {
+            list1.map((item,index)=>
+            <li key={index}>
+              <input
+              type='checkbox'
+              checked={item.checked}
+              onChange={()=>handleCheckboxChange(index)}/>
+              {item.title}
+            </li>
+            )
+          }
+        </ul>
+      </div>
+      <div>
+        <h3>List 2</h3>
+        <ul>
+          {
+            list2.map((item,index)=>
+            <li key={index}>
+              {item.title}
+            </li>
+            )
+          }
+        </ul>
+      </div>
+      <button onClick={handleSwap}>swap the item</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
